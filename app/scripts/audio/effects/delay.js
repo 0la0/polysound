099e36915a446d9b698e7e3ea353d46719858aa6
Luke Anderson <luke.kansas@gmail.com>
2016-01-30 22:08:50 -0600

    adding delay UI

--- app/scripts/audio/effects/delay.js
+++ app/scripts/audio/effects/delay.js
@@ -1,31 +1,37 @@
 import BaseEffect from './BaseEffect.js';
-//import generateUniqueId from '../util/uniqueGenerator.js';
 
 export default class Delay extends BaseEffect {
 
   constructor (audioContext) {
     super(audioContext);
-
     this.input = this.audioContext.createDelay();
     this.feedback = this.audioContext.createGain();
     this.wetLevel = this.audioContext.createGain();
 
-    this.input.delayTime.value = 0.25; //250 ms delay
     this.feedback.gain.value = 0.85;
     this.wetLevel.gain.value = 0.80;
+    this.connectDelayNodes();
+  }
 
+  setQuatization (tempo, delayTime) {
+    this.disconnectDelayNodes();
+    setTimeout(() => {
+      this.connectDelayNodes();
+      this.input.delayTime.value = (tempo / 1000) / delayTime;
+    }, 0);
+  }
+
+  disconnectDelayNodes () {
+    this.input.disconnect(this.wetLevel);
+    this.input.disconnect(this.feedback);
+    this.wetLevel.disconnect(this.mainGain);
+  }
+
+  connectDelayNodes () {
     this.wetLevel.connect(this.mainGain);
     this.feedback.connect(this.input);
     this.input.connect(this.wetLevel);
     this.input.connect(this.feedback);
   }
-
-  setQuatization (tempo, delayTime) {
-    //var temp = this.feedback.gain.value;
-  	//this.feedback.gain.value = 0;
-    this.input.delayTime.value = (tempo / 1000) / delayTime;
-  	//this.feedback.gain.value = temp;
-  }
-
 
 }
