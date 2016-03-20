25e0b027d49015526aeaada5e8052124d97f0e49
Luke Anderson <luke.kansas@gmail.com>
2016-03-20 16:53:40 -0500

    adding more waveshaping functions and starting waveshaper UI node

--- app/scripts/audio/effects/waveshaper.js
+++ app/scripts/audio/effects/waveshaper.js
@@ -3,6 +3,7 @@
  *  https://developer.mozilla.org/en-US/docs/Web/API/WaveShaperNode
  *  https://github.com/Theodeus/tuna/blob/master/tuna.js (specifically waveshaperAlgorithms section)
  *  http://music.columbia.edu/cmc/MusicAndComputers/chapter4/04_06.php
+ *  http://msp.ucsd.edu/techniques/v0.11/book-html/node78.html
  */
 
 import BaseEffect from './BaseEffect.js';
@@ -12,24 +13,47 @@ export default class Waveshaper extends BaseEffect {
   constructor (audioContext, buffer) {
     super(audioContext);
     this.input = this.audioContext.createGain();
-
     this.waveshaperNode = this.audioContext.createWaveShaper();
-    this.waveshaperNode.curve = createDistortionCurve(50, this.audioContext.sampleRate);
-
     this.input.connect(this.waveshaperNode);
     this.waveshaperNode.connect(this.mainGain);
   }
 
-}
-
-function createDistortionCurve (amount, sampleRate) {
-  let curve = new Float32Array(sampleRate);
-  let degree = Math.PI / 180;
-
-  for (let i = 0; i < sampleRate; ++i) {
-    let x = i * 2 / sampleRate - 1;
-    curve[i] = ( 3 + amount ) * x * 20 * degree / ( Math.PI + amount * Math.abs(x) );
+  getCarrierFunctions () {
+    return Object.keys(CARRIER_FUNCTIONS);
   }
 
+  setCarrierFunction (functionKey) {
+    let carrierFunction = CARRIER_FUNCTIONS[functionKey];
+    (carrierFunction) ?
+      this.waveshaperNode.curve = createCurve(carrierFunction, this.audioContext.sampleRate, 50) :
+      console.warn('Waveshkaper.setCarrierFunction error, invalid function key');
+  }
+
+}
+
+function createCurve (carrierFunction, sampleRate, multiplier) {
+  let curve = new Float32Array(sampleRate);
+  for (let i = 0; i < sampleRate; i++) {
+    //adjust x such that the curve is centered in its domain
+    let x = i * 2 / sampleRate - 1;
+    //sigmoid with fast dropoff, range is (-1, 1)
+    curve[i] = carrierFunction(x, multiplier);
+  }
   return curve;
 }
+
+const CARRIER_FUNCTIONS = {
+
+  Square: (x) => {
+    return Math.pow(x, 2);
+  },
+
+  Sigmoid: (x) => {
+    return 2 / (1 + Math.exp(-4 * x)) - 1;
+  },
+
+  SigmoidLike: (x, multiplier) => {
+    return ( 3 + multiplier ) * x * 20 * (Math.PI / 180) / ( Math.PI + multiplier * Math.abs(x) );
+  }
+
+};
