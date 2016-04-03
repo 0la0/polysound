af077ffad0bc1087536fe5b046927a5817a8e01a
Luke Anderson <luke.kansas@gmail.com>
2016-04-02 23:13:19 -0500

    adding gain to each synth

--- app/scripts/audio/instruments/synthContainer.js
+++ app/scripts/audio/instruments/synthContainer.js
@@ -8,31 +8,49 @@ export default class SynthContainer extends BaseInstrument {
     super(audioContext);
 
     this.synthList = [
-      new Synth(this.audioContext, this.semitoneRatio),
-      new Synth(this.audioContext, this.semitoneRatio),
-      new Synth(this.audioContext, this.semitoneRatio)
+      createSynthNode(this.audioContext, this.semitoneRatio),
+      createSynthNode(this.audioContext, this.semitoneRatio),
+      createSynthNode(this.audioContext, this.semitoneRatio)
     ];
+
+    this.synthList.forEach(synth => synth.gain.connect(this.output));
   }
 
   play (pitch, schedule) {
     let sampleLength = this.adsr.attack + this.adsr.sustain + this.adsr.release;
     let adsrEnvelope = adsrBuilder(this.audioContext, this.output, schedule, this.adsr);
-    this.synthList.forEach(synth => synth.play(pitch, schedule, adsrEnvelope, sampleLength));
+    this.synthList.forEach(obj => {
+      let adsrEnvelope = adsrBuilder(this.audioContext, obj.gain, schedule, this.adsr);
+      obj.synth.play(pitch, schedule, adsrEnvelope, sampleLength);
+    });
   }
 
   setOscilator (index, type) {
     if (index < 0 || index >= this.synthList.length) {
       throw new Error('index out of bounds, SynthContainer.setOscilator', arguments);
     }
-    this.synthList[index].setOscilator(type);
+    this.synthList[index].synth.setOscilator(type);
   }
 
   start () {
-    this.synthList.forEach(synth => synth.start(this.output));
+    this.synthList.forEach((obj) => {
+      obj.synth.start(obj.gain);
+    });
   }
 
   stop () {
-    this.synthList.forEach(synth => synth.stop(this.output));
+    this.synthList.forEach((obj) => {
+      obj.synth.stop(obj.gain);
+    });
   }
 
+}
+
+function createSynthNode (audioContext, semitoneRatio) {
+  let gainNode = audioContext.createGain();
+  gainNode.gain.value = 0.5;
+  return {
+    synth: new Synth(audioContext, semitoneRatio),
+    gain: gainNode
+  };
 }
