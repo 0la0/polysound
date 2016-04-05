2f2b0291be6d9c94730af2cee971b1e640ff0eb4
Luke Anderson <luke.kansas@gmail.com>
2016-04-04 22:58:30 -0500

    more robust chorus

--- app/scripts/audio/effects/chorus.js
+++ app/scripts/audio/effects/chorus.js
@@ -1,30 +1,41 @@
+// modeled after modDelay: https://github.com/cwilso/Audio-Input-Effects/blob/master/js/effects.js
+
 import BaseEffect from './BaseEffect.js';
 
-export default class Chorus extends BaseEffect {
+export default class Chorus extends BaseEffect  {
 
   constructor (audioContext) {
     super(audioContext);
 
     this.input = this.audioContext.createGain();
 
-    this.delay = this.audioContext.createDelay();
-    this.delay.delayTime.value = 0.2; //delay => input.delayTime.value
+    this.delayNode = this.audioContext.createDelay();
+    this.delayNode.delayTime.value = 0.1; //delayTime
+
+    this.feedbackGain = this.audioContext.createGain();
+    this.feedbackGain.gain.value = 0.1; //feedback
+
+    this.chorus = this.audioContext.createDelay();
+    this.chorus.delayTime.value = 0.03; //chorus delay
 
     this.lfo = this.audioContext.createOscillator();
     this.lfo.type = 'sine';
-    this.lfo.frequency.value = 0.8; //speed => lfo frequency
+    this.lfo.frequency.value = 0.5; //chorus speed
 
     this.lfoGain = this.audioContext.createGain();
-    this.lfoGain.gain.value = 0.8; //depth => gain.gain.value
+    this.lfoGain.gain.value = 0.003; //chorus depth
 
     this.lfo.connect(this.lfoGain);
-    this.lfoGain.connect(this.delay.delayTime);
+    this.lfoGain.connect(this.chorus.delayTime);
+
+    this.input.connect(this.delayNode);
+    this.delayNode.connect(this.chorus);
+    this.delayNode.connect(this.feedbackGain);
+    this.chorus.connect(this.feedbackGain);
+    this.feedbackGain.connect(this.delayNode);
+    this.feedbackGain.connect(this.mainGain);
+
     this.lfo.start(0);
-
-    this.delay.connect(this.mainGain);
-    this.input.connect(this.mainGain);
-    this.input.connect(this.delay);
   }
-
 
 }
